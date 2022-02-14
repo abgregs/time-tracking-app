1d0e6679d0b8e1311fefbee049d951dd2599e8b5
Austin Gregersen <abgregs@gmail.com>
2022-02-14 12:10:29 -0700

    add mui components and styles

--- src/App.js
+++ src/App.js
@@ -5,19 +5,42 @@ import {
   findById,
   renderElapsedString } from './helpers.js';
 
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+import { useTheme } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import EditIcon from '@mui/icons-material/Edit';
+import Button from '@mui/material/Button';
+import Input from '@mui/material/Input';
+import AddIcon from '@mui/icons-material/Add';
+import InputLabel from '@mui/material/InputLabel';
+import ButtonGroup from '@mui/material/ButtonGroup';
+  
 function App() {
+
+  const theme = useTheme();
 
   class TimersDashboard extends React.Component {
     render() {
       return (
-        <div className='centered-grid'>
+        <Container maxWidth='sm'>
           <div className='column'>
             <EditableTimerList />
             <ToggleableTimerForm
               isOpen={true}
             />
           </div>
-        </div>
+        </Container>
       );
     }
   }
@@ -30,9 +53,9 @@ function App() {
         );
       } else {
         return (
-          <div className='plus-button-container'>
-            { /* TODO: add plus icon for create new timer button */ }
-          </div>
+          <Box sx={{textAlign: 'center' }}>
+            <Button variant='contained'><AddIcon></AddIcon></Button>
+          </Box>
         );
       }
     }
@@ -87,27 +110,27 @@ function App() {
     render() {
       const elapsedString = renderElapsedString(this.props.elapsed);
       return (
-        <div className='centered-card'>
-          <div className='card-content'>
-            <div className='header'>
-              {this.props.title}
-            </div>
-            <div className='meta'>
-              {this.props.project}
-            </div>
-            <div className='description'>
-              <h2>
-                {elapsedString}
-              </h2>
-            </div>
-            <div className='timer-options'>
-              { /* TODO: add buttons for deleting or editing timer */ }
-            </div>
-          </div>
-          <div className='ui bottom attached blue basic button'>
-            Start
-          </div>
-        </div>
+        <Card variant='outlined' sx={{ textAlign: 'center', mb: 4 }}>
+          <CardContent>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Box sx={{ display: 'flex', flexDirection: 'column', textAlign: 'left', flexWrap: 'wrap' }}>
+                <Typography component='div' variant='h5'>
+                  {this.props.title}
+                </Typography>
+                <Typography variant='subtitle1' color='text.secondary' component='div'>
+                  {this.props.project}
+                </Typography>
+              </Box>
+              <Box>
+                <EditIcon></EditIcon><DeleteForeverIcon></DeleteForeverIcon>
+              </Box>
+            </Box>
+            <Typography variant='h4' sx={{ textAlign: 'center', mb: 2 }} color='text.primary' component='div'>
+              {elapsedString}
+            </Typography>
+            <Button variant='contained'>Start</Button>
+          </CardContent>
+        </Card>
       );
     }
   }
@@ -116,28 +139,22 @@ function App() {
     render() {
       const submitText = this.props.title ? 'Update' : 'Create';
       return (
-        <div className='ui centered card'>
-          <div className='content'>
-            <div className='form'>
-              <div className='field'>
-                <label>Title</label>
-                <input type='text' defaultValue={this.props.title} />
-              </div>
-              <div className='field'>
-                <label>Project</label>
-                <input type='text' defaultValue={this.props.project} />
-              </div>
-              <div className='bottom-buttons-container'>
-                <button className='button submit-button'>
-                  {submitText}
-                </button>
-                <button className='button cancel-button'>
-                  Cancel
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Card variant='outlined' sx={{ mb: 4 }}>
+          <CardContent>
+            <Box sx={{ mb: 2 }}>
+                <InputLabel htmlFor='component-simple'>Title</InputLabel>
+                <Input id='component-simple' value={this.props.title} sx={{ mb: 2 }}/>
+                <InputLabel htmlFor='component-simple'>Project</InputLabel>
+                <Input id='component-simple' value={this.props.project} sx={{ mb: 2 }}/>
+            </Box>
+            <Box sx={{ textAlign: 'center' }}>
+              <ButtonGroup disableElevation>
+                <Button variant='contained'>{submitText}</Button>
+                <Button variant='outlined'>Cancel</Button>
+              </ButtonGroup>
+            </Box>
+          </CardContent>
+        </Card>
       );
     }
   }
@@ -145,12 +162,12 @@ function App() {
   
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="app-title">Timers</h1>
+    <div className='App'>
+      <header className='App-header'>
+        <Typography variant='h2' sx={{ textAlign: 'center', fontWeight: 300 }} my={2} className='app-title'>Timers</Typography>
       </header>
-        <div id="main" className="main">
-          <div id="content">
+        <div id='main' className='main'>
+          <div id='content'>
             <TimersDashboard />,
           </div>
         </div>
